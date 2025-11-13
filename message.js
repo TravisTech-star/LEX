5f289309ab6057e9a3e5bad3e080d389bc40e2af
TravisTech-star <nyekotravis95@gmail.com>
2025-11-13 23:08:17 +0300

    Create message.js

---
+++ message.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const MessageSchema = new mongoose.Schema({
+    sender:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    receiver:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    text:{type:String}
+},{timestamps:true});
+module.exports = mongoose.model('Message', MessageSchema);
